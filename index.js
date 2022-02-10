556b4ced3b60beab5d6c592fc2aac71212351520
Beatrix Mátlé <matlebeatrix@edu.bme.hu>
2022-02-10 14:11:27 +0100

    add flash msg feature

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 //write the code here
+window.onload = () => {
+    const flashes = document.querySelectorAll('.flash');
+    setInterval(() => {
+        for (const flash of flashes) {
+            flash.classList.toggle('red');
+        }
+    }, 1000)
+}
